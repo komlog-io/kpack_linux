b28404c8c3e6c4178639b3a22bd3f6b2c35153f6
juan cañete <jcazor@komlog.org>
2017-10-16 10:40:06 +0200

    v0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name = 'kpack_linux',
     license = 'Apache Software License',
     packages = ['kpack_linux'],
-    version = '0.1',
+    version = '0.2',
     entry_points = {
         'komlogd.package': 'load = kpack_linux.load'
     }
